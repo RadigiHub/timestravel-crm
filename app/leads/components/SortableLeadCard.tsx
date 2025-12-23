b5fee7d1a0afb4b917014bc77d2b124c84277177
RadigiHub <teamradmarketing@gmail.com>
2025-12-23 17:43:33 +0000

    Update SortableLeadCard.tsx

--- app/leads/components/SortableLeadCard.tsx
+++ app/leads/components/SortableLeadCard.tsx
@@ -1,42 +1,43 @@
 "use client";
 
-import * as React from "react";
-import { CSS } from "@dnd-kit/utilities";
+import React from "react";
 import { useSortable } from "@dnd-kit/sortable";
-import type { Lead } from "../types";
+import { CSS } from "@dnd-kit/utilities";
+
 import LeadCard from "./LeadCard";
+import type { Lead } from "../types";
 
 type Props = {
+  id: string;
+  statusId: string;
   lead: Lead;
 };
 
-export default function SortableLeadCard({ lead }: Props) {
+export default function SortableLeadCard({ id, statusId, lead }: Props) {
   const {
     attributes,
     listeners,
     setNodeRef,
-    setActivatorNodeRef,
     transform,
     transition,
     isDragging,
-  } = useSortable({ id: lead.id });
+  } = useSortable({
+    id,
+    data: { statusId }, // IMPORTANT: used in Board to know fromStatus
+  });
 
   const style: React.CSSProperties = {
     transform: CSS.Transform.toString(transform),
     transition,
-    opacity: isDragging ? 0.6 : 1,
+    opacity: isDragging ? 0.4 : 1,
   };
 
   return (
     <div ref={setNodeRef} style={style}>
       <LeadCard
         lead={lead}
-        dragHandleRef={setActivatorNodeRef}
-        dragHandleProps={{
-          ...attributes,
-          ...listeners,
-          style: { touchAction: "none" },
-        }}
+        handleAttributes={attributes as any}
+        handleListeners={listeners}
       />
     </div>
   );
